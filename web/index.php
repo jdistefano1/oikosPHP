4170a37411d279cbcb13e24e258156cbf1f9092f
jdistefano1 <jdistefano@mtb.com>
2016-02-15 17:02:00 -0500

    1

--- web/index.php
+++ web/index.php
@@ -3,8 +3,9 @@
 <form action="charge.php" method="post">
   <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
           data-key="<?php echo $stripe['publishable_key']; ?>"
-          data-description="Rental Fees Payment"
-		  data-name="Oikos Homes Renatal Charge"
+          data-description="Rental Payment"
+		  data-name="Oikos Homes"
           data-amount="5000"
-          data-locale="auto"></script>
+          data-locale="auto">
+		  Data-image = ./logo.png</script>
 </form>
