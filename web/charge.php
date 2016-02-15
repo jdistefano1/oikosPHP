3b910c3bd2955741f0c88a010077656dd43b5263
jdistefano1 <jdistefano@mtb.com>
2016-02-15 17:47:19 -0500

    a

--- web/charge.php
+++
@@ -1,6 +0,0 @@
-<?php
-  require_once(dirname(__FILE__) . '/config.php');
-
-  $token  = $_POST['stripeToken'];
-  
-  echo '<h1>Successfully charged $50.00!</h1>';
--- web/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('../vendor/stripe/lib/stripe.php');
-
-$stripe = array(
-  "secret_key"      => "sk_test_vrlJzvYx3uPbyLVsGboMg5h0",
-  "publishable_key" => "pk_test_lXReVaKu10izIVP0RRmg0y5q"
-);
-
-\Stripe\Stripe::setApiKey($stripe['secret_key']);
