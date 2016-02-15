56724c9ec4c84ec6cbea8e7ba076cc0e3efef1eb
jdistefano1 <jdistefano@mtb.com>
2016-02-15 17:36:19 -0500

    a

--- web/charge.php
+++ web/charge.php
@@ -2,9 +2,5 @@
   require_once(dirname(__FILE__) . '/config.php');
 
   $token  = $_POST['stripeToken'];
-
-  
-
-  var_dump($customer->keys() );
   
   echo '<h1>Successfully charged $50.00!</h1>';
--- web/index.php
+++ web/index.php
@@ -1,13 +1,22 @@
 <?php require_once('./config.php'); ?>
 
+
 <form action="charge.php" method="post">
+ <input type="number" id="custom-donation" placeholder="1000.00" min="1.00"  />
+  <script> ('#custom-donation').onchange(function(){
+      // Stripe accepts payment amounts in cents so we have to convert dollars to cents by multiplying by 100
+     var amount = parseInt( $(this).val()*100);
+     $(".stripe-button").attr( "data-amount", amount );}
+  <script/>
   <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
           data-key="<?php echo $stripe['publishable_key']; ?>"
           data-description="Rental Payment"
 		  data-name="Oikos Homes"
-          data-amount="25000"
+          data-amount="0"
           data-locale="auto"
 		  data-label="Make Payment"
 		  >
   </script>
 </form>
+<
+$
