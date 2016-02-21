26d0e944c996cc716623ce56919b8e9f2a57fe2e
jdistefano1 <jdistefano@mtb.com>
2016-02-21 09:10:54 -0500

    4

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-require_once('vendor/autoload.php');
+require_once('stripe/init.php');
 $stripe = array(
-  secret_key      => getenv('secret_key'),
-  publishable_key => getenv('publishable_key')
+  secret_key      => getenv('sk_test_vrlJzvYx3uPbyLVsGboMg5h0'),
+  publishable_key => getenv('pk_test_lXReVaKu10izIVP0RRmg0y5q')
 );
 \Stripe\Stripe::setApiKey($stripe['secret_key']);
 ?>
