eb5532845d7cb09d9fb3ad81a71381a752d826cf
jdistefano1 <jdistefano@mtb.com>
2016-02-21 14:32:28 -0500

    q

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
-//require_once('./header.php');
+
 //require_once('./payment_form.php');
 //require_once('./footer.php');
 
 echo "Hello world!"; 
+<?php require_once('../config.php'); ?>
+
 ?>
