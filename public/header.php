836f1ff2589d8e802ceed26782c790d866b7bb38
jdistefano1 <jdistefano@mtb.com>
2016-02-21 14:39:24 -0500

    2

--- public/header.php
+++ public/header.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Wilde Things</title>
+    <title>Oikos Homes</title>
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/style.css">
   </head>
--- public/index.php
+++ public/index.php
@@ -1,8 +1,6 @@
 <?php
-
-//require_once('./payment_form.php');
-//require_once('./footer.php');
-
-echo "Hello world!"; 
 require_once('/header.php');
+require_once('/payment_form.php');
+require_once('/footer.php');
+
 ?>
