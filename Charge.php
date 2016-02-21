faa60dcfeafb029e8365a015c1fc81dbad7fadb2
jdistefano1 <jdistefano@mtb.com>
2016-02-21 12:57:02 -0500

    1

---
+++ Charge.php
@@ -0,0 +1,29 @@
+<?php require_once('./config.php'); ?>
+
+<form action="charge.php" method="post">
+  <script src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+          data-key="<?php echo $stripe['publishable_key']; ?>"
+          data-description="Access for a year"
+          data-amount="5000"
+          data-locale="auto"></script>
+</form>
+
+
+<?php
+  require_once('./config.php');
+
+  $token  = $_POST['stripeToken'];
+
+  $customer = \Stripe\Customer::create(array(
+      'email' => 'customer@example.com',
+      'card'  => $token
+  ));
+
+  $charge = \Stripe\Charge::create(array(
+      'customer' => $customer->id,
+      'amount'   => 5000,
+      'currency' => 'usd'
+  ));
+
+  echo '<h1>Successfully charged $50.00!</h1>';
+?>
