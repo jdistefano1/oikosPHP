b039b87786335a2f777378d3c21e09bf1b16b8f0
jdistefano1 <jdistefano@mtb.com>
2016-02-21 14:46:01 -0500

    s

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
-require_once('/header.php');
-require_once('/payment_form.php');
-require_once('/footer.php');
+require_once('./header.php');
+require_once('./payment_form.php');
+require_once('./footer.php');
 
 ?>
