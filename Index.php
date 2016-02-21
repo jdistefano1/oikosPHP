6405c5988df5207e069b7f57d21fef2912594a88
jdistefano1 <jdistefano@mtb.com>
2016-02-21 08:54:49 -0500

    3

--- Index.php
+++ Index.php
@@ -13,10 +13,10 @@ $stripe = array(
   'secret_key'      => 'sk_test_vrlJzvYx3uPbyLVsGboMg5h0',
   'publishable_key' => 'pk_test_lXReVaKu10izIVP0RRmg0y5q'
   );
-\Stripe\Stripe::setApiKey($stripe['secret_key']);
+\Stripe\lib\Stripe::setApiKey($stripe['secret_key']);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  $charge = \Stripe\Charge::create(array(
+  $charge = \Stripe\lib\Charge::create(array(
     'source'     => $_POST['stripeToken'],
     'amount'   => 53500,
     'currency' => 'usd'
