24677175fc716ef669e2f51428f1c0adee23a21a
jdistefano1 <jdistefano@mtb.com>
2016-03-01 19:30:15 -0500

    s

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-require_once('public\vendor\autoload.php');
+require_once('d:\home\site\wwwroot\public\vendor\autoload.php');
 
 $stripe = array(
   "secret_key"      => "sk_test_vrlJzvYx3uPbyLVsGboMg5h0",
   "publishable_key" => "pk_test_lXReVaKu10izIVP0RRmg0y5q"
 );
 
-public\Stripe\Stripe::setApiKey($stripe['secret_key']);
+d:\home\site\wwwroot\public\Stripe\Stripe::setApiKey($stripe['secret_key']);
 ?>
