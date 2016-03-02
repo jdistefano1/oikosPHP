651fec6f33f878e93c7a81cd7d3ca6e6537e39ae
jdistefano1 <jdistefano@mtb.com>
2016-03-01 20:08:34 -0500

    s

--- public/payment_form - Copy.php
+++ public/payment_form - Copy.php
@@ -1,13 +1,11 @@
 <h3>Sign up and buy a quote now! Only $535.00!</h3>
 <form action="charge.php" method="POST">
 	<input type="password" name="password" placeholder="Password" />
-	<input type="amount" name="amount" placeholder="Amount" />
-	
   <script
     src="https://checkout.stripe.com/checkout.js" class="stripe-button"
     data-key="<?php echo $stripe['publishable_key']; ?>"
     data-description="One Wilde quote"
-    data-amount=$Amount>
+    data-amount="53500">
   </script>
 </form>
 <hr />
