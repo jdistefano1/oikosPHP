b841eef955c4ef18770f4bd5b5baa80463ec874c
jdistefano1 <jdistefano@mtb.com>
2016-03-07 18:10:03 -0500

    d

--- public/config.php
+++ public/config.php
@@ -1,7 +1,7 @@
 <?php
 require_once('public/vendor/autoload.php');
 $stripe = array(
-	'secret_key'      => '<YOUR SECRET STRIPE API KEY>',
-	'publishable_key' => '<YOUR PUBLISHABLE STRIPE API KEY>'
+	'secret_key'      => 'sk_test_vrlJzvYx3uPbyLVsGboMg5h0',
+	'publishable_key' => 'pk_test_lXReVaKu10izIVP0RRmg0y5q'
 );
 \Stripe\Stripe::setApiKey($stripe['secret_key']);
--- public/payment_form.php
+++ public/payment_form.php
@@ -1,15 +1,8 @@
-<h3>Make a Payment</h3>
 <form action="charge.php" method="POST">
-	
-	<input type="password" name="password" placeholder="Password" />
-	<input type="amount" name="amount" placeholder="amount" />
-	
   <script
     src="https://checkout.stripe.com/checkout.js" class="stripe-button"
     data-key="<?php echo $stripe['publishable_key']; ?>"
-    data-description="Oikos Homes Payment"
-    data-amount=<?php echo $amount ;?>
+    data-description="One Wilde quote"
+    data-amount="53500">
   </script>
 </form>
-<hr />
-<?php require_once('./login_form.php'); ?>
