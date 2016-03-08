9062dd2955419ef7ff5c9b1a22292f4ef78c8e79
jdistefano1 <jdistefano@mtb.com>
2016-03-07 19:17:00 -0500

    Revert "Revert "s""
    This reverts commit 9697b2fdd54ee31844582538205bc998728ac3d9.

--- public/Charge.php
+++ public/Charge.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $charge = \Stripe\Charge::create(array(
       'source'     => $_POST['stripeToken'],
-      'amount'   => 53500,
+      'amount'   => $_POST['amount'],
       'currency' => 'usd'
     ));
 
