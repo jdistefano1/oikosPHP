4ca6557d6da83ccf021256f87fb00e254d2c3ad3
jdistefano1 <jdistefano@mtb.com>
2016-03-07 19:32:28 -0500

    s

--- public/payment_form.php
+++ public/payment_form.php
@@ -4,7 +4,7 @@
   <script
     src="https://checkout.stripe.com/checkout.js" class="stripe-button"
     data-key="<?php echo $stripe['publishable_key']; ?>"
-    data-description="One Wilde quote"
+    data-description="Oikos Homes Payment"
     data-amount="<?php echo $_POST['amount']; ?>">
   </script>
 </form>
