2ea67cac722a3afeccbde59d7103a8eb935a9b29
jdistefano1 <jdistefano@mtb.com>
2016-03-07 19:11:16 -0500

    s

--- public/payment_form.php
+++ public/payment_form.php
@@ -1,6 +1,9 @@
 <form action="charge.php" method="POST">
 
 
+Payment Amount: <input type="text" name="amount"><br>
+
+
   <script
     src="https://checkout.stripe.com/checkout.js" class="stripe-button"
     data-key="<?php echo $stripe['publishable_key']; ?>"
